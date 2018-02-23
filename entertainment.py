9ca0b26ba61361ab6c556dc89d1cf814eb43858c
Mayank <mayank_gulati@stu.upes.ac.in>
2018-02-23 15:58:37 +0530

    first file

--- entertainment.py
+++ entertainment.py
@@ -1,3 +1,4 @@
 import media
 toy_story=media.Movie("Toystory","boy's toys turn alive","https://www.amazon.com/Toy-Story-Tim-Allen/dp/B0049J0DC8","https://www.youtube.com/watch?v=KYz2wyBy3kc")
 print(toy_story.trailer)
+print(toy_story.poster)

55cba4c3892d219d26f2264c42b9b4bc9a774bf8
Mayank <mayank_gulati@stu.upes.ac.in>
2018-02-23 15:45:01 +0530

    first file

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self,movie_title,movie_story_line,movie_poster,movie_youtube_trailer):
+        self.title=movie_title
+        self.storline=movie_story_line
+        self.poster=movie_poster
+        self.trailer=movie_youtube_trailer
